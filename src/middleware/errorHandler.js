890948911c59e6b321070ff0f7a03bcb43b727a4
Svitlana <svetlanayurevna98@gmail.com>
2025-12-04 06:38:45 +0000

    Fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,15 @@
+import { HttpError } from "http-errors";
+
 export const errorHandler = (err, req, res, _next) => {
   req.log?.error(err);
 
-  const status = err.status || 500;
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
-  res.status(status).json({
-    message: err.message || "Server error",
+  res.status(500).json({
+    message: "Internal Server Error",
   });
 };
